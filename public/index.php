8921b17058e3e3b82e5cfcc78b0b9b4f7e0a1cc3
William <work@suppo.fr>
2021-01-07 13:28:48 +0100

    Exemple de retour de facture.

--- public/index.php
+++ public/index.php
@@ -7,3 +7,10 @@ require __DIR__.'/../vendor/autoload.php';
 $car = new Car('Peugeot 205');
 
 dump($car);
+
+// Format attendu de la facture du panier
+
+$facture = [
+    "Peugoet 205, Sapin Fraicheur océan", // voir => https://www.php.net/manual/en/function.implode
+    505,
+];
